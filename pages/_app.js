c1d53bd04b811b51e44608708f0838ff833713c8
Muhammad Syahid bin Nor Azman <syahid@time.com.my>
2022-06-11 09:44:18 +0800

    Designing Main page layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import { ThemeProvider } from 'next-themes'
+import siteMetadata from '../data/siteMetadata'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
+import { PageSEO } from '../components/SEO'
+import ThemeSwitch from '../components/ThemeSwitch'
+import siteMetadata from '../data/siteMetadata'
+
+
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+  <>
+    <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
+    <div className="grid grid-cols-1 gap-4 p-4 md:grid-cols-3">
+      <div className="shadow-lg shadow-black bg-slate-100 rounded-md p-4 dark:bg-transparent hover:shadow">test1</div>
+      <div className="col-span-2 rounded-md p-4">test2</div>
+      <div className="col-span-2 rounded-md p-4"></div>
+    </div>
+    <ThemeSwitch />
+  </>
   )
 }
